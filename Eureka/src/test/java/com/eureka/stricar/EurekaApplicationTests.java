95836de336b9c19d6083066f293ded230d1604c6
Rahul Kumar Singh <singhr@hyd.eclature.com>
2020-02-11 18:47:08 +0530

    new

---
+++ Eureka/src/test/java/com/eureka/stricar/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eureka.stricar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.eureka.stricar.EurekaApplication;
+
+@SpringBootTest(classes={EurekaApplication.class})
+class EurekaApplicationTests {
+
+@Test
+	void contextLoads() {
+	}
+
+}
